97a9bc970215ba83d4c865c4caea077d31fc259e
arvis <info@yourgurunet.com>
2021-02-18 14:52:54 +0200

    Create new activity in the list

--- API/Controllers/ActivitiesContoller.cs
+++ API/Controllers/ActivitiesContoller.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -9,17 +8,24 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-
+        // Get List
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await Mediator.Send(new List.Query());
         }
-
+        // Get one
         [HttpGet("{id}")] //activities/id
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<ActionResult<Activity>> GetActivity(string id)
         {
             return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        // Create
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
     }
 }
--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -11,7 +11,7 @@ namespace Application.Activities
     {
         public class Query : IRequest<Activity>
         {
-            public Guid Id { get; set; }
+            public string Id { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, Activity>
