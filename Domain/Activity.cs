1829bbb2a2ac056cf2eb8b3b375b00c812c03199
arvis <info@yourgurunet.com>
2021-03-05 14:09:03 +0200

    Required title attribute

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
     public class Activity
     {
         public string Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
