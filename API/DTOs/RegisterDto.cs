48b28d2706fa5d2e27ea7b2d1726d121ddadd3c6
arvis <info@yourgurunet.com>
2021-03-19 14:24:47 +0200

    Validating the registration of users
    * RegisterDto.cs

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$",
+         ErrorMessage = "One of the characters needs to be number," +
+         "one lowercase, and one uppercase character, and from 4-8 total long.")]
         public string Password { get; set; }
+
+        [Required]
         public string UserName { get; set; }
     }
 }
