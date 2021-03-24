d7b10dca17d9ab156f4bda6abee91eb601819d11
arvis <info@yourgurunet.com>
2021-03-24 13:54:13 +0200

    Shaping the related data * List.cs: * Profile.cs: * MappingProfiles.cs: * ActivityDto.cs:

---
+++ Application/Activities/ActivityDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Application.Profiles;
+
+namespace Application.Activities
+{
+    public class ActivityDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+        public string HoustUsername { get; set; }
+
+        public ICollection<Profile> Profiles {get; set;}
+    }
+}
