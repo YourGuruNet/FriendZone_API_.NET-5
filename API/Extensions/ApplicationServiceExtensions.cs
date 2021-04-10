510e7e68ca52dd8246a722d34bc75f94b79a18ea
arvis <info@yourgurunet.com>
2021-04-10 14:53:24 +0300

    Adding the Cloudinary interfaces: * PhotoAccessor.cs: * PhotoUploadResult.cs: * IPhotoAccessor.cs: * ApplicationServiceExtensions.cs:

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -35,6 +35,7 @@ namespace API.Extensions
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, IPhotoAccessor>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             return services;
---
+++ Application/Photos/PhotoAccessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Photos
+{
+    public class PhotoAccessor : IPhotoAccessor
+    {
+        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
